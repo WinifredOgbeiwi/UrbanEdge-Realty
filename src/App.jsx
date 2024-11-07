29f585613da317bb4babfb668325eb1994102477
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-11-07 22:52:36 +0100

    restructure route links and made navbar restricted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/layouts/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Landing_page/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -15,26 +15,34 @@ import Listings from "./pages/Landing_page/Listings";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PrivateRoute from "./routes/PrivateRoute";
+import { ROUTES } from "./utils/routes";
 const App = () => {
-  
+  const location = useLocation();
+  const showNavbar = [ROUTES.HOME, ROUTES.LISTINGS];
+
+  const shouldShowNavbar = showNavbar.includes(location.pathname);
+
   return (
     <>
-      <Navbar />
+     {shouldShowNavbar &&  <Navbar /> }
 
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset-password" element={<ForgotPassword />} />
-        <Route path="/confirm-registration" element={<ConfirmRegistration />} />
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.LISTINGS} element={<Listings />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
+        <Route path={ROUTES.REGISTER} element={<Register />} />
+        <Route path={ROUTES.RESET_PASSWORD} element={<ForgotPassword />} />
+        <Route
+          path={ROUTES.CONFIRM_REGISTRATION}
+          element={<ConfirmRegistration />}
+        />
         <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
+          <Route path={ROUTES.PROFILE} element={<Profile />} />
         </Route>
 
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/sell" element={<Sell />} />
-        <Route path="/property:id" element={<PropertiesDetails />} />
+        <Route path={ROUTES.BUY} element={<Buy />} />
+        <Route path={ROUTES.SELL} element={<Sell />} />
+        <Route path={ROUTES.PROPERTY} element={<PropertiesDetails />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ToastContainer />
---
+++ src/utils/routes.js
@@ -0,0 +1,21 @@
+export const ROUTES = {
+  // web
+  HOME: "/",
+  LISTINGS: "/listings",
+
+  // AUTHENTICATION
+  LOGIN: "/login",
+  REGISTER: "/register",
+  RESET_PASSWORD: "/reset-password",
+  CONFIRM_REGISTRATION: "/confirm-registration",
+
+  //DASHBOARD
+  PROFILE: "/profile",
+  BUY: "/buy",
+  SELL: "/sell",
+  PROPERTY: "/property:id",
+};
+
+
+
+  
